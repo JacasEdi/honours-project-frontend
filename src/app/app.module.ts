a177a9dd09e94afd3b35fa2057cc16ce3249191a
JacasEdi <jacek.budzynski.92@gmail.com>
2019-02-16 12:53:34 +0000

    Allow GraphQL and REST requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,66 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
 import { LearnerComponent } from './learner/learner.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+
+import {
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule, MatProgressSpinnerModule,
+  MatRadioModule,
+  MatSelectModule, MatTableModule,
+  MatTabsModule
+} from '@angular/material';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'profile',
+    component: LearnerComponent
+  },
+  {
+    path: 'courses',
+    component: CoursesComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    LearnerComponent
+    LearnerComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     GraphQLModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
+    MatFormFieldModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatIconModule,
+    MatInputModule,
+    MatTabsModule,
+    MatTableModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/learner/learner.component.ts
+++ src/app/learner/learner.component.ts
@@ -1,4 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { Subscription } from 'apollo-client/util/Observable';
+import { HttpClient } from '@angular/common/http';
+
+
+const LEARNER_QUERY = gql`
+  query{
+    learner(id: "5c5dd1056957460c4ff04015") {
+      firstName
+      lastName
+      email
+      age
+      enrolments {
+        progress
+        course {
+          title
+        }
+        createdAt
+        updatedAt
+      }
+    }
+  }
+`;
 
 @Component({
   selector: 'app-learner',
@@ -6,10 +31,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./learner.component.css']
 })
 export class LearnerComponent implements OnInit {
+  options: FormGroup;
+  displayedColumns: string[] = ['title', 'progress', 'createdAt', 'updatedAt'];
+  learner: any;
+  private querySubscription: Subscription;
 
-  constructor() { }
+  constructor(private apollo: Apollo, private http: HttpClient, fb: FormBuilder) {
+    this.options = fb.group({
+      hideRequired: false,
+      floatLabel: 'auto',
+    });
+  }
 
   ngOnInit() {
+      this.getLearnerWithGraphql();
+  }
+
+  getLearnerWithRest() {
+      this.http.get<any>('http://localhost:3100/learners/5c5dd1056957460c4ff04015').subscribe(data => {
+        this.learner = data;
+      });
+  }
+
+  getLearnerWithGraphql() {
+    this.querySubscription = this.apollo.watchQuery<any>({ query: LEARNER_QUERY }).valueChanges
+      .subscribe(({ data }) => {
+        this.learner = data.learner;
+      });
   }
 
 }
